7331457bce4f86d2cc967c50cd36f25f04c32bc3
marisahleleshep <124134341+marisahleleshep@users.noreply.github.com>
2023-06-14 23:09:18 +0300

    index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// Get references to the input elements and task list
+
 const addInput = document.getElementById('addInput');
 const deleteInput = document.getElementById('deleteInput');
 const pendingTasksInput = document.getElementById('pendingTasksInput');
 const taskList = document.getElementById('taskList');
 
-// Function to add a new task
+
 function addTask() {
   const newTask = addInput.value.trim();
   if (newTask !== '') {
@@ -16,7 +16,7 @@ function addTask() {
   }
 }
 
-// Function to delete tasks
+
 function deleteTask() {
   const taskToDelete = deleteInput.value.trim();
   const taskItems = taskList.getElementsByTagName('li');
@@ -24,13 +24,12 @@ function deleteTask() {
     if (taskItems[i].textContent === taskToDelete) {
       taskItems[i].remove();
       updatePendingTasks();
-      break; // Stop searching after deleting the first matching task
+      break; 
     }
   }
   deleteInput.value = '';
 }
 
-// Function to update the pending tasks input
 function updatePendingTasks() {
   const taskItems = taskList.getElementsByTagName('li');
   let pendingTasks = '';
@@ -40,13 +39,11 @@ function updatePendingTasks() {
   pendingTasksInput.value = pendingTasks.trim();
 }
 
-// Function to clear pending tasks
 function clearPendingTasks() {
   taskList.innerHTML = '';
   pendingTasksInput.value = '';
 }
 
-// Function to complete a task
 function completeTask() {
   const taskToComplete = completeInput.value.trim();
   const taskItems = taskList.getElementsByTagName('li');
@@ -54,17 +51,16 @@ function completeTask() {
     if (taskItems[i].textContent === taskToComplete) {
       taskItems[i].classList.add('completed');
       updatePendingTasks();
-      break; // Stop searching after completing the first matching task
+      break; 
     }
   }
   completeInput.value = '';
 }
 
-// Attach click event listeners to the buttons
 document.getElementById('addButton').addEventListener('click', addTask);
 document.getElementById('deleteButton').addEventListener('click', deleteTask);
 document.getElementById('clearButton').addEventListener('click', clearPendingTasks);
-// Attach click event listener to the complete button
+
 document.getElementById('completeButton').addEventListener('click', completeTask);
 
 
